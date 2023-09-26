fd952dfbddf09f543d02137d6a174775ff7cc10d
ouchaou <yaminaouchaou20@gmail.com>
2023-09-26 14:54:32 +0200

    add database confing

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  'cloud_sb29', // Database name
+  'clouduser', // User
+  'Uo85L7ID3jS1MBZulGCwBlDnCJ0qeJtp', // Password
   {
-    host: '', // Host
+    host: '@dpg-ck9d0r1ehpqs73c3nj8g-a', // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
